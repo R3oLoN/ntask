078189820a6f5cd08abb9b75162cb5c065286b2f
Leandro Reolon <leandroreolon@gmail.com>
2017-07-12 10:35:50 -0300

    Organização da rotas da API e carregamento via  consign

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 import express from "express";
+import consign from "consign";
 const PORT = 3000;
 const app = express();
 
 app.set('json spaces', 4);
 
-app.get("/", (req, res) => res.json({ status: 'NTask API' }));
-
-app.get("/tasks", (req, res) => {
-    res.json({
-        tasks: [
-            { title: "Fazer compras" },
-            { title: "Consertar o PC" }
-        ]
-    });
-});
+consign()
+    .include('routes')
+    .into(app);
 
 app.listen(PORT, () => console.log(`NTask API - porta ${PORT}`));
