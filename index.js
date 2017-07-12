3ef6f6d8656c17c95abba89442ac4d9fa50bba34
Leandro Reolon <leandroreolon@gmail.com>
2017-07-12 10:42:35 -0300

    Criação da pasta para os modelos e configuração do Tasks

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app = express();
 app.set('json spaces', 4);
 
 consign()
-    .include('routes')
+    .include('models')
+    .then('routes')
     .into(app);
 
 app.listen(PORT, () => console.log(`NTask API - porta ${PORT}`));
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,10 +1,8 @@
 module.exports = app => {
+    const Tasks = app.models.tasks;
     app.get("/tasks", (req, res) => {
-        res.json({
-            tasks: [
-                { title: "Fazer compras" },
-                { title: "Consertar o PC" }
-            ]
+        Tasks.findAll({}, (tasks) =>{
+            res.json({ tasks: tasks});
         });
     });
 }
