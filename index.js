588136036cef5e971ee9e86a61b87fdf7fce0ad7
Leandro Reolon <leandroreolon@gmail.com>
2017-07-12 10:28:16 -0300

    Configurando a API para retornar JSON formatado

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 const PORT = 3000;
 const app = express();
 
+app.set('json spaces', 4);
+
 app.get("/", (req, res) => res.json({ status: 'NTask API' }));
 
 app.get("/tasks", (req, res) => {
