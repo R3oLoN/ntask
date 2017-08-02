4d0dbbefa230e516b4908007187ce45deaaa3e4e
R3oLoN <you@example.com>
2017-08-02 11:26:45 -0300

    Configuração para geração de documentação da api

--- libs/middleware.js
+++ libs/middleware.js
@@ -1,8 +1,10 @@
 import bodyParser from 'body-parser';
+import express from 'express';
 
 module.exports = app => {
     app.set('port', 3000);
     app.set('json spaces', 4);
     app.use(bodyParser.json());
     app.use(app.libs.auth.initialize());
+    app.use(express.static('public'));
 }
