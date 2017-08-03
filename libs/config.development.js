10e9d11445bec042916d95d596c9b2e581659ae7
R3oLoN <leandroreolon@gmail.com>
2017-08-03 18:34:06 -0300

    Configuração de logs de banco e request

--- libs/config.development.js
+++ libs/config.development.js
@@ -1,8 +1,12 @@
+import logger from './logger.js';
+
 module.exports = {
     database: 'mongodb://localhost/ntask',
     username: '',
     password: '',
-    logging: true,
+    logging: (coll, method, query, fields) => {
+        logger.info(`collection: ${coll}, method: ${method}, query: ${JSON.stringify(query)}, fields: ${JSON.stringify(fields)}`);
+    },
     jwtSecret: 'Nt@$K-AP1',
     jwtSession: { session: false }
 }
