48ab255db8f9f586b2c68179664de6f23d2e262b
R3oLoN <leandroreolon@gmail.com>
2017-08-03 16:16:48 -0300

    Configuração do CORS

--- libs/middleware.js
+++ libs/middleware.js
@@ -1,10 +1,18 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import cors from 'cors';
 
 module.exports = app => {
-    app.set('port', 3000);
-    app.set('json spaces', 4);
-    app.use(bodyParser.json());
-    app.use(app.libs.auth.initialize());
-    app.use(express.static('public'));
+	app.set('port', 3000);
+	if (!process.env.NODE_ENV) {
+		app.set('json spaces', 4);
+	}
+	app.use(cors({
+		origin: ['http://localhost:3001'],
+		methods: ['GET', 'POST', 'PUT', 'DELETE'],
+		allowedHeaders: ['Content-Type', 'Authorization']
+	}));
+	app.use(bodyParser.json());
+	app.use(app.libs.auth.initialize());
+	app.use(express.static('public'));
 }
